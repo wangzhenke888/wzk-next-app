9112bd279d75f07ec5b0ccc5f62d2fefb6ec453b
王振科 <913769712@qq.com>
2025-09-03 02:02:40 +0800

    测试vercel的CICD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Home() {
   return (
     <div>
-      <h1 style={{ textAlign: "center", marginBottom: "24px" }}>wzk的next项目</h1>
+      <h1 style={{ textAlign: "center", marginBottom: "24px" }}>测试vercel的CICD——wzk的next项目</h1>
     </div>
   )
 }
